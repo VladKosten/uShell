566c401a0fbf2521c4ad0f95523eb5f016ead86c
Vlad Kosten <vladkosten@gmail.com>
2025-03-14 23:17:24 +0300

    Refactor UShell command module for improved readability and consistency

--- main/ushell_cmd.c
+++ main/ushell_cmd.c
@@ -1,21 +1,20 @@
 /**
-* \file         UShell_cmd.c
-* @brief        The file contains the implementation of the UShell command module.
-*               Object UShellCmd is a parent object for the command objects.
-* \authors      Vladislav Kosten (vladkosten@gmail.com)
-* \copyright
-* \warning      A warning may be placed here...
-* \bug          Bug report may be placed here...
-*/
+ * \file         ushell_cmd.c
+ * @brief        The file contains the implementation of the UShell command module.
+ *               Object UShellCmd is a parent object for the command objects.
+ * \authors      Vladislav Kosten (vladkosten@gmail.com)
+ * \copyright
+ * \warning      A warning may be placed here...
+ * \bug          Bug report may be placed here...
+ */
 //===============================================================================[ INCLUDE ]========================================================================================
 
 #include "ushell_cmd.h"
-
 //=====================================================================[ INTERNAL MACRO DEFINITIONS ]===============================================================================
 
 /**
-* @brief Assert macro for the UShellHal module.
-*/
+ * @brief Assert macro for the UShellHal module.
+ */
 #ifndef USHELL_CMD_ASSERT
     #ifdef USHELL_ASSERT
         #define USHELL_CMD_ASSERT(cond) USHELL_ASSERT(cond)
@@ -31,32 +30,32 @@
 //=======================================================================[ PUBLIC INTERFACE FUNCTIONS ]=============================================================================
 
 /**
- * @brief Initialize the UShell cmd  module.
+ * \brief Initialize the UShell cmd  module.
  * \param [in] cmd - UShellCmd obj to be initialized
- * \param [in] argMax - maximum number of arguments of the cmd
- * \param [in] portable - portable structure for the command
- * \param [in] portTable - port table to be used
  * \param [in] name - name of the object
  * \param [in] parent - pointer to the parent object
+ * \param [in] help - help string
+ * \param [in] portable - pointer to the portable table
  * \param [out] none
- * @return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdInit(UShellCmd_s* const cmd, size_t argMax,
-                                                     const UShellCmdPortable_s* const  portable,
-                                                     const char* const name,
-                                                     const void* const parent)
+ * \return UShellOsalErr_e - error code
+ */
+UShellCmdErr_e UShellCmdInit(UShellCmd_s* const cmd,
+                             const char* const name,
+                             const void* const parent,
+                             const UShellCmdHelp_t* const help,
+                             const UShellCmdPortTable_s* const portable)
 {
-    /* Check input parametr */
-    if((NULL == cmd) ||
-       (NULL == name) ||
-       (NULL == parent) ||
-       (NULL == portable))
+    /* Check input parameter */
+    if ((NULL == cmd) ||
+        (NULL == name) ||
+        (NULL == parent) ||
+        (NULL == portable))
     {
         return USHELL_CMD_INVALID_ARGS_ERR;
     }
 
     /* Init the object */
-    memset((void*)cmd, 0, sizeof(UShellCmd_s));
+    memset((void*) cmd, 0, sizeof(UShellCmd_s));
 
     /* Set the name */
     cmd->name = name;
@@ -64,8 +63,8 @@ UShellCmdErr_e UShellCmdInit(UShellCmd_s* const cmd, size_t argMax,
     /* Set the parent */
     cmd->parent = parent;
 
-    /* Set the maximum number of arguments */
-    cmd->argMax = argMax;
+    /* Set the help string */
+    cmd->help = help;
 
     /* Set the portable structure */
     cmd->portable = portable;
@@ -78,63 +77,19 @@ UShellCmdErr_e UShellCmdInit(UShellCmd_s* const cmd, size_t argMax,
  * \param [in] cmd - UShellOsal obj to be deinitialized
  * \param [out] none
  * @return UShellOsalErr_e - error code
-*/
+ */
 UShellCmdErr_e UShellCmdDeinit(UShellCmd_s* const cmd)
 {
-    /* Check input parametr */
-    if(NULL == cmd)
+    /* Check input parameter */
+    if (NULL == cmd)
     {
         return USHELL_CMD_INVALID_ARGS_ERR;
     }
 
     /* Deinit the object */
-    memset((void*)cmd, 0, sizeof(UShellCmd_s));
+    memset((void*) cmd, 0, sizeof(UShellCmd_s));
 
     return USHELL_CMD_NO_ERR;
-}
-
-/**
- * @brief Attach the print function to the UShellCmd
- * \param [in] cmd - UShellCmd obj
- * \param [in] print - pointer to the function to print a string
- * \param [out] none
- * @return UShellCmdErr_e - error code
-*/
-UShellCmdErr_e UShellCmdPrintCbAttach(UShellCmd_s* const cmd, const UShellCmdPrintCb_t print)
-{
-    /* Check input parametr */
-    if((NULL == cmd) ||
-       (NULL == print))
-    {
-        return USHELL_CMD_INVALID_ARGS_ERR;
-    }
-
-    /* Attach the print function */
-    cmd->print = print;
-
-    return USHELL_CMD_NO_ERR;
-
-}
-
-/**
- * @brief Detach the print function from the UShellCmd
- * \param [in] cmd - UShellCmd obj
- * \param [out] none
- * @return UShellCmdErr_e - error code
-*/
-UShellCmdErr_e UShellCmdPrintCbDetach(UShellCmd_s* const cmd)
-{
-    /* Check input parametr */
-    if(NULL == cmd)
-    {
-        return USHELL_CMD_INVALID_ARGS_ERR;
-    }
-
-    /* Detach the print function */
-    cmd->print = NULL;
-
-    return USHELL_CMD_NO_ERR;
-
 }
 
 /**
@@ -142,12 +97,13 @@ UShellCmdErr_e UShellCmdPrintCbDetach(UShellCmd_s* const cmd)
  * \param [in] cmd - UShellOsal obj
  * \param [out] name - name of the object
  * @return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdNameGet(UShellCmd_s* const cmd, char** const name)
+ */
+UShellCmdErr_e UShellCmdNameGet(UShellCmd_s* const cmd,
+                                char** const name)
 {
-    /* Check input parametr */
-    if((NULL == cmd) ||
-       (NULL == name))
+    /* Check input parameter */
+    if ((NULL == cmd) ||
+        (NULL == name))
     {
         return USHELL_CMD_INVALID_ARGS_ERR;
     }
@@ -159,90 +115,24 @@ UShellCmdErr_e UShellCmdNameGet(UShellCmd_s* const cmd, char** const name)
 }
 
 /**
- * @brief Get the parent of the UShell  module.
- * \param [in] cmd - UShellOsal obj
- * \param [out] parent - pointer to the parent object
- * @return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdArgMaxGet(UShellCmd_s* const cmd, size_t* const argMax)
-{
-    /* Check input parametr */
-    if((NULL == cmd) ||
-       (NULL == argMax))
-    {
-        return USHELL_CMD_INVALID_ARGS_ERR;
-    }
-
-    /* Get the parent */
-    *argMax = cmd->argMax;
-
-    return USHELL_CMD_NO_ERR;
-
-}
-
-/**
- * @brief Execute the UShell module.
- * \param [in] cmd - UShellOsal obj
- * \param [in] argc - number of arguments
- * \param [in] argv - arguments
- * \param [out] none
- * @return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdExecute(UShellCmd_s* const cmd, const int argc, const char* const argv[])
-{
-    /* Check input parametr */
-    if((NULL == cmd) ||
-       (NULL == argv))
-    {
-        return USHELL_CMD_INVALID_ARGS_ERR;
-    }
-
-    /* Check init state */
-    if((cmd->portable == NULL) ||
-       (cmd->portable->execute == NULL))
-    {
-        return USHELL_CMD_NOT_INIT_ERR;
-    }
-
-    /* Check the number of arguments */
-    if(argc > cmd->argMax)
-    {
-        return USHELL_CMD_EXECUTE_NUMB_ARGS_ERR;
-    }
-
-    /* Execute the command */
-    UShellCmdErr_e err = cmd->portable->execute(cmd, argc, argv);
-
-    return err;
-}
-
-/**
  * @brief Get the help string of the cmd.
  * \param [in] cmd - UShellOsal obj
  * \param [out] help - help string
  * @return UShellOsalErr_e - error code
-*/
+ */
 UShellCmdErr_e UShellCmdHelpGet(UShellCmd_s* const cmd, UShellCmdHelp_t** const help)
 {
-    /* Check input parametr */
-    if((NULL == cmd) ||
-       (NULL == help))
+    /* Check input parameter */
+    if ((NULL == cmd) ||
+        (NULL == help))
     {
-        return USHELL_CMD_INVALID_ARGS_ERR;      //< Invalid arguments
-    }
-
-    /* Check init state */
-    if((cmd->portable == NULL) ||
-       (cmd->portable->help == NULL))
-    {
-        return USHELL_CMD_NOT_INIT_ERR;
+        return USHELL_CMD_INVALID_ARGS_ERR;    //< Invalid arguments
     }
 
     /* Get the help string */
+    *help = cmd->help;
 
-    *help = cmd->portable->help;
-
-    return USHELL_CMD_NO_ERR;       //< Success
+    return USHELL_CMD_NO_ERR;    //< Success
 }
 
 //============================================================================ [PRIVATE FUNCTIONS ]=================================================================================
--- main/ushell_cmd.h
+++ main/ushell_cmd.h
@@ -3,7 +3,7 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif  /* __cplusplus */
+#endif /* __cplusplus */
 
 /*================================================================[INCLUDE]================================================*/
 
@@ -11,8 +11,6 @@ extern "C" {
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-
-#include "ushell_osal.h"
 
 /*===========================================================[MACRO DEFINITIONS]============================================*/
 
@@ -23,157 +21,105 @@ extern "C" {
  */
 typedef enum
 {
-    USHELL_CMD_NO_ERR = 0,            ///< Exit: no errors (success)
-    USHELL_CMD_INVALID_ARGS_ERR,      ///< Exit: error - invalid pointers (e.g. null pointers)
-    USHELL_CMD_NOT_INIT_ERR,          ///< Exit: error - not initialized
-    USHELL_CMD_PORT_ERR,              ///< Exit: error - port error (e.g. port layer error)
-    USHELL_CMD_EXECUTE_NUMB_ARGS_ERR, ///< Exit: error - number of arguments error
+    USHELL_CMD_NO_ERR = 0,               ///< Exit: no errors (success)
+    USHELL_CMD_INVALID_ARGS_ERR,         ///< Exit: error - invalid pointers (e.g. null pointers)
+    USHELL_CMD_NOT_INIT_ERR,             ///< Exit: error - not initialized
+    USHELL_CMD_PORT_ERR,                 ///< Exit: error - port error (e.g. port layer error)
+    USHELL_CMD_EXECUTE_NUMB_ARGS_ERR,    ///< Exit: error - number of arguments error
+
 } UShellCmdErr_e;
 
 /**
- * \brief Prototype of the function to execute a command.
-*/
-typedef UShellCmdErr_e (*UShelCmdExecute_t)( const void* const cmd, const int argc, const char* const argv[]);
-
-/**
- * \brief Portable structure for the command
-*/
-typedef struct
-{
-    UShellCmdErr_e (*run)(const void* const cmd, const int argc, const char* const argv[]);     ///< Pointer to the function to execute the command
-    UShellCmdErr_e (*stop)(const void* const cmd);                                              ///< Pointer to the function to stop the execution of the command
-}UShellCmdPortable_s;
-
-/**
- * \brief Callback table
-*/
-typedef struct
-{
-    UShellCmdErr_e (*print)(const void* const cmd, const char* const str, const UShellCmdTextColor_e color);    ///< Pointer to the function to print a string
-    UShellCmdErr_e (*endOfExecution)(const void* const cmd);                                                    ///< Pointer to the function to signal the end of the execution
-
-}UShellCmdCb_s;
-
-
-typedef enum
-{
-    USHELL_CMD_TEXT_COLOR_BLACK = 0,    ///< Black color
-    USHELL_CMD_TEXT_COLOR_RED,          ///< Red color
-    USHELL_CMD_TEXT_COLOR_GREEN,        ///< Green color
-    USHELL_CMD_TEXT_COLOR_YELLOW,       ///< Yellow color
-    USHELL_CMD_TEXT_COLOR_BLUE,         ///< Blue color
-    USHELL_CMD_TEXT_COLOR_WHITE,        ///< White color
-
-}UShellCmdTextColor_e;
-
-/**
- * \brief Describe a cmd help string
-*/
+ * \brief Describe a cmd help string.
+ */
 typedef char* UShellCmdHelp_t;
 
 /**
- * \brief Descriibe UShellCmd
-*/
+ * @brief Describe size of one item in the UShell.
+ */
+typedef char UShellCmdItem_t;
+
+/**
+ * @brief Describe a cmd port table.
+ */
+typedef struct
+{
+    /**
+     * @brief Execute cmd
+     * \param[in] cmd - pointer to the cmd object
+     * \param[in] arg - pointer to the arguments (Can be NULL and it means no arguments)
+     * \param[in] out - pointer to the output buffer (Can be NULL and it means no output)
+     */
+    UShellCmdErr_e (*exec)(void* const cmd,
+                           const UShellCmdItem_t* const arg,
+                           UShellCmdItem_t* const out);
+} UShellCmdPortTable_s;
+
+/**
+ * \brief Describe UShellCmd.
+ *
+ * This structure represents a UShell command.
+ *
+ * \param parent Pointer to the parent object (ushell object).
+ * \param name Pointer to the name of the command.
+ * \param help Pointer to the help string (must be a valid string).
+ */
 typedef struct
 {
     /* Mandatory */
-    const void* parent;                     ///< Pointer to the parent object(ushell object)
-    const char* name;                       ///< Pointer to the name of the object(aka command)
-    size_t argMax;                          ///< Maximum number of arguments
-    const UShellOsal_s* osal;               ///< Pointer to the osal object
-    const UShellCmdHelp_t* help;            ///< Pointer to the help string
-    const UShellCmdPortable_s* portTable;   ///< Portable structure for the command
-    UShellCmdCb_s cbTable;                  ///< Pointer to the function to print a string
+    const void* parent;                      ///< Pointer to the parent object
+    const char* name;                        ///< Pointer to the name of the object (aka command)
+    const UShellCmdHelp_t* help;             ///< Pointer to the help string (Must be string)
+    const UShellCmdPortTable_s* portable;    ///< Pointer to the port table
 
-}UShellCmd_s;
-
+} UShellCmd_s;
 
 /*===========================================================[PUBLIC INTERFACE]=============================================*/
 
 /**
  * \brief Initialize the UShell cmd  module.
  * \param [in] cmd - UShellCmd obj to be initialized
- * \param [in] argMax - maximum number of arguments of the cmd
- * \param [in] portable - portable structure for the command
- * \param [in] portTable - port table to be used
  * \param [in] name - name of the object
  * \param [in] parent - pointer to the parent object
+ * \param [in] help - help string
+ * \param [in] portable - pointer to the portable table
  * \param [out] none
  * \return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdInit(UShellCmd_s* const cmd, size_t argMax,
-                                                     const UShellCmdPortable_s* const portable,
-                                                     const char* const name);
+ */
+UShellCmdErr_e UShellCmdInit(UShellCmd_s* const cmd,
+                             const char* const name,
+                             const void* const parent,
+                             const UShellCmdHelp_t* const help,
+                             const UShellCmdPortTable_s* const portable);
 
 /**
  * \brief Deinitialize the UShell  module.
  * \param [in] cmd - UShellOsal obj to be deinitialized
  * \param [out] none
  * \return UShellOsalErr_e - error code
-*/
+ */
 UShellCmdErr_e UShellCmdDeinit(UShellCmd_s* const cmd);
-
-/**
- * \brief Attach the print function to the UShellCmd
- * \param [in] cmd - UShellCmd obj
- * \param [in] print - pointer to the function to print a string
- * \param [out] none
- * \return UShellCmdErr_e - error code
-*/
-UShellCmdErr_e UShellCmdPrintCbAttach(UShellCmd_s* const cmd, const UShellCmdPrintCb_f print);
-
-/**
- * \brief Detach the print function from the UShellCmd
- * \param [in] cmd - UShellCmd obj
- * \param [out] none
- * \return UShellCmdErr_e - error code
-*/
-UShellCmdErr_e UShellCmdPrintCbDetach(UShellCmd_s* const cmd);
 
 /**
  * \brief Get the name of the UShell  module.
  * \param [in] cmd - UShellOsal obj
  * \param [out] name - name of the object
  * \return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdNameGet(UShellCmd_s* const cmd, char** const name);
-
-/**
- * \brief Get the parent of the UShell  module.
- * \param [in] cmd - UShellOsal obj
- * \param [out] parent - pointer to the parent object
- * \return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdArgMaxGet(UShellCmd_s* const cmd, size_t* const argMax);
-
-/**
- * \brief Execute the UShell module.
- * \param [in] cmd - UShellOsal obj
- * \param [in] argc - number of arguments
- * \param [in] argv - arguments
- * \param [out] none
- * \return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdRun(UShellCmd_s* const cmd, const int argc, const char* const argv[]);
-
-/**
- * \brief Stop the execution of the UShell module.
- * \param [in] cmd - UShellOsal obj
- * \param [out] none
- * \return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdStop(UShellCmd_s* const cmd);
+ */
+UShellCmdErr_e UShellCmdNameGet(UShellCmd_s* const cmd,
+                                char** const name);
 
 /**
  * \brief Get the help string of the cmd.
  * \param [in] cmd - UShellOsal obj
  * \param [out] help - help string
  * \return UShellOsalErr_e - error code
-*/
-UShellCmdErr_e UShellCmdHelpGet(UShellCmd_s* const cmd, UShellCmdHelp_t** const help);
+ */
+UShellCmdErr_e UShellCmdHelpGet(UShellCmd_s* const cmd,
+                                UShellCmdHelp_t** const help);
 
 #ifdef __cplusplus
 }
-#endif  /* __cplusplus */
+#endif /* __cplusplus */
 
-#endif  /* USHELL_CMD_H_ */
+#endif /* USHELL_CMD_H_ */
